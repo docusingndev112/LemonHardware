3764fa1fd5f7b45e8803f8e511f4efbfa564f250
Niraj Modi <nirajmodi@Nirajs-MacBook-Air.local>
2024-01-11 12:45:07 +0530

    Initial commit

---
+++ Example/LemonHardware/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LemonHardware
+//
+//  Created by docusingndev112 on 01/11/2024.
+//  Copyright (c) 2024 docusingndev112. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/LemonHardware/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LemonHardware
+//
+//  Created by docusingndev112 on 01/11/2024.
+//  Copyright (c) 2024 docusingndev112. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
